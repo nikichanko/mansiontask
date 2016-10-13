5f667285ae473912cbcab057b6d416bf3d57c64e
Nikolay Nikolov <Nikolay Nikolov>
2016-10-13 16:51:12 +0300

    added extend function in script js

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,31 @@ Mansion.Functions.addEvents = function(el){
             console.log('266');
         }, false);
     }
-
 }
+
+Mansion.Functions.extend = function (defaults, options) {
+    this.defaults = defaults;
+    this.options = options;
+    this.extended = {};
+
+    for (var prop in this.defaults) {
+        if (Object.prototype.hasOwnProperty.call(this.defaults, prop)) {
+            this.extended[prop] = this.defaults[prop];
+        }
+    }
+    for (var prop in this.options) {
+        if (Object.prototype.hasOwnProperty.call(this.options, prop)) {
+            this.extended[prop] = this.options[prop];
+        }
+    }
+};
 
 var el = document.getElementById('niki');
 var obj = new Mansion.Functions.addEvents(el);
 obj.apply();
+
+var defaults = {'event1':1,'event2':2,'event2':3};
+var options = {'evat3':4};
+var obj1 = new  Mansion.Functions.extend(defaults, options);
+var return1 = obj1.extended;
+console.log(return1);
